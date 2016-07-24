a2dccd8418990f94fce0820e97ccbeccca596fe6
Alex Grintsvayg <alex@grin.io>
2016-07-24 10:57:36 -0400

    update for cloudflare api v4

--- ddns.php
+++ ddns.php
@@ -12,7 +12,7 @@ if (!file_exists($confFile))
 
 $config = require $confFile;
 
-foreach(array('cloudflare_email','cloudflare_api_key','domain','record_name','ttl','cloudflare_active', 'protocol') as $key)
+foreach (['cloudflare_email', 'cloudflare_api_key', 'domain', 'record_name', 'ttl', 'protocol'] as $key)
 {
   if (!isset($config[$key]) || $config[$key] === '')
   {
@@ -23,35 +23,39 @@ foreach(array('cloudflare_email','cloudflare_api_key','domain','record_name','tt
 
 $api = new Cloudflare($config['cloudflare_email'], $config['cloudflare_api_key']);
 
-$domain = $config['domain'];
+$domain     = $config['domain'];
 $recordName = $config['record_name'];
 
 $ip = getIP($config['protocol']);
 
 $verbose = !isset($argv[1]) || $argv[1] != '-s';
 
-try 
+try
 {
-  $record = getExistingRecord($api, $domain, $recordName, $verbose);
+  $zone = $api->getZone($domain);
+  if (!$zone)
+  {
+    echo "domain $domain not found\n";
+    return 1;
+  }
+
+  $records = $api->getZoneDnsRecords($zone['id'], ['name' => $recordName]);
+  $record  = $records && $records[0]['name'] == $recordName ? $records[0] : null;
+
   if (!$record)
   {
     if ($verbose) echo "No existing record found. Creating a new one\n";
-    $ret = $api->rec_new($domain, 'A', $recordName, $ip, $config['ttl'], $config['cloudflare_active']);
-    throwExceptionIfError($ret);
+    $ret = $api->createDnsRecord($zone['id'], 'A', $recordName, $ip, ['ttl' => $config['ttl']]);
   }
-  elseif($record['type'] != 'A')
+  elseif ($record['type'] != 'A' || $record['content'] != $ip || $record['ttl'] != $config['ttl'])
   {
-    if ($verbose) echo "Record exists but is not an A record. Fixing that.\n";
-    $ret = $api->rec_delete($domain, $record['rec_id']);
-    throwExceptionIfError($ret);
-    $ret = $api->rec_new($domain, 'A', $recordName, $ip, $config['ttl'], $config['cloudflare_active']);
-    throwExceptionIfError($ret);
-  }
-  elseif($record['content'] != $ip)
-  {
-    if ($verbose) echo "Record exists. Updating to the new IP address.\n";
-    $ret = $api->rec_edit($domain, 'A', $record['rec_id'], $ip, $config['ttl'], $config['cloudflare_active']);
-    throwExceptionIfError($ret);
+    if ($verbose) echo "Updating record.\n";
+    $ret = $api->updateDnsRecord($zone['id'], $record['id'], [
+      'type'    => 'A',
+      'name'    => $recordName,
+      'content' => $ip,
+      'ttl'     => $config['ttl'],
+    ]);
   }
   else
   {
@@ -61,7 +65,7 @@ try
 }
 catch (Exception $e)
 {
-  echo "Error: " . $e->message . "\n";
+  echo "Error: " . $e->getMessage() . "\n";
   return 1;
 }
 
@@ -70,46 +74,10 @@ catch (Exception $e)
 // http://major.io/icanhazip-com-faq/
 function getIP($protocol)
 {
-  $prefixes = array('ipv4' => 'ipv4.', 'ipv6' => 'ipv6.', 'auto' => '');
+  $prefixes = ['ipv4' => 'ipv4.', 'ipv6' => 'ipv6.', 'auto' => ''];
   if (!isset($prefixes[$protocol]))
   {
     throw new Exception('Invalid "protocol" config value.');
   }
   return trim(file_get_contents('http://' . $prefixes[$protocol] . 'icanhazip.com'));
-}
-
-
-function getExistingRecord($api, $domain, $recordName, $verbose)
-{
-  if ($verbose) echo "Getting existing record.\n";
-  $retry = false;
-  $count = 0;
-  do {
-    $count++;
-    $data = $api->rec_load_all($domain);
-    $retry = isset($data['error']) && $data['error'] == 'Operation timed out after 5001 milliseconds with 0 bytes received';
-
-    if ($retry && $count > 5)
-    {
-      throw new Exception('Could not get data from Cloudflare after 5 retries. Aborting.');
-    }
-  } while ($retry);
-
-  foreach($data['response']['recs']['objs'] as $record)
-  {
-    if ($record['name'] == $recordName)
-    {
-      return $record;
-    }
-  }
-  return null;
-}
-
-
-function throwExceptionIfError($data)
-{
-  if (isset($data['error']))
-  {
-    throw new Exception($data['error']);
-  }
 }
