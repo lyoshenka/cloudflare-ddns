4d166504c819be6f4a643189dcc92663ee50d549
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2019-10-08 16:13:19 +0200

    re-format long ifs and arrays for better readability

--- ddns.php
+++ ddns.php
@@ -11,7 +11,14 @@ if (!file_exists($confFile)) {
 
 $config = require $confFile;
 
-foreach (['cloudflare_email', 'cloudflare_api_key', 'domain', 'record_name', 'ttl', 'protocol'] as $key) {
+foreach ([
+             'cloudflare_email',
+             'cloudflare_api_key',
+             'domain',
+             'record_name',
+             'ttl',
+             'protocol',
+         ] as $key) {
     if (!isset($config[$key]) || $config[$key] === '') {
         echo "config.php is missing the '$key' config value\n";
         return 1;
@@ -20,12 +27,16 @@ foreach (['cloudflare_email', 'cloudflare_api_key', 'domain', 'record_name', 'tt
 
 $api = new Cloudflare($config['cloudflare_email'], $config['cloudflare_api_key']);
 
-$domain     = $config['domain'];
+$domain = $config['domain'];
 $recordName = $config['record_name'];
 
 if (isset($config['auth_token']) && $config['auth_token']) {
     // API mode. Use IP from request params.
-    if (empty($_GET['auth_token']) || empty($_GET['ip']) || $_GET['auth_token'] != $config['auth_token']) {
+    if (
+        empty($_GET['auth_token']) ||
+        empty($_GET['ip']) ||
+        $_GET['auth_token'] != $config['auth_token']
+    ) {
         echo "Missing or invalid 'auth_token' param, or missing 'ip' param\n";
         return 1;
     }
@@ -45,23 +56,29 @@ try {
     }
 
     $records = $api->getZoneDnsRecords($zone['id'], ['name' => $recordName]);
-    $record  = $records && $records[0]['name'] == $recordName ? $records[0] : null;
+    $record = $records && $records[0]['name'] == $recordName ? $records[0] : null;
 
     if (!$record) {
         if ($verbose) {
             echo "No existing record found. Creating a new one\n";
         }
-        $ret = $api->createDnsRecord($zone['id'], 'A', $recordName, $ip, ['ttl' => $config['ttl']]);
-    } elseif ($record['type'] != 'A' || $record['content'] != $ip || $record['ttl'] != $config['ttl']) {
+        $ret = $api->createDnsRecord($zone['id'], 'A', $recordName, $ip, [
+            'ttl' => $config['ttl'],
+        ]);
+    } elseif (
+        $record['type'] != 'A' ||
+        $record['content'] != $ip ||
+        $record['ttl'] != $config['ttl']
+    ) {
         if ($verbose) {
             echo "Updating record.\n";
         }
         $ret = $api->updateDnsRecord($zone['id'], $record['id'], [
-      'type'    => 'A',
-      'name'    => $recordName,
-      'content' => $ip,
-      'ttl'     => $config['ttl'],
-    ]);
+            'type' => 'A',
+            'name' => $recordName,
+            'content' => $ip,
+            'ttl' => $config['ttl'],
+        ]);
     } else {
         if ($verbose) {
             echo "Record appears OK. No need to update.\n";
@@ -73,12 +90,15 @@ try {
     return 1;
 }
 
-
 // http://stackoverflow.com/questions/3097589/getting-my-public-ip-via-api
 // http://major.io/icanhazip-com-faq/
 function getIP($protocol)
 {
-    $prefixes = ['ipv4' => 'ipv4.', 'ipv6' => 'ipv6.', 'auto' => ''];
+    $prefixes = [
+        'ipv4' => 'ipv4.',
+        'ipv6' => 'ipv6.',
+        'auto' => '',
+    ];
     if (!isset($prefixes[$protocol])) {
         throw new Exception('Invalid "protocol" config value.');
     }
